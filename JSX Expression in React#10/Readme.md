9a07b5ef7cea16a8ea65105d2c286c30a0c91ff6
ISRA ILYAS <israqureshi138@gmail.com>
2024-11-11 00:02:19 +0500

    template literals

--- JSX Expression in React#10/Readme.md
+++ JSX Expression in React#10/Readme.md
@@ -1,4 +1,9 @@
-# JSX Expression {}
+## JSX Expression {}
 
 - we can use {} if we want to add variable or expressions in jsx elements in react app.
 - We can only use expression in {} not statements (eg conditional statements like if else)
+
+## Template Literals
+
+- Using backticks `` 
+- eg `<h1>{`My Name is ${fName} ${lName}`}</h1>`
--- JSX Expression in React#10/myreactapp/src/index.js
+++ JSX Expression in React#10/myreactapp/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const mName = "ISRA ILYAS";
+// const mName = "ISRA ILYAS";
 const age = 18;
+const fName = "ISRA";
+const lName="ILYAS";
 
 ReactDOM.render(
+  // ===== JSX Expression In React  =====
+  // <>
+  //   <h1>JSX Expression</h1>
+  //   <p>My name is {mName}</p>
+  //   <p>My age is :{age}</p>
+  //   <p>Random number: {Math.random()}</p>
+  // </>,
+
+  // === Template Literals In React ====
   <>
-    <h1>JSX Expression</h1>
-    <p>My name is {mName}</p>
-    <p>My age is :{age}</p>
-    <p>Random number: {Math.random()}</p>
+{/* <h1>My Name is {fName +" "+ lName}</h1> */}
+<h1>{`My Name is ${fName} ${lName}`}</h1>
   </>,
   document.getElementById('root')
 )
