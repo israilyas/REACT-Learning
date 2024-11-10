10b2b659eeecb4853e5a9bff4553ce5b771b5d42
ISRA ILYAS <israqureshi138@gmail.com>
2024-11-10 23:15:41 +0500

    studied how to render multiple jsx elements and completed react challenge 1

--- #7 Render multiple/rendermultiple/src/index.js
+++ #7 Render multiple/rendermultiple/src/index.js
@@ -9,12 +9,25 @@ ReactDOM.render(
   // </div>
 
   // React version above 16 accept array of jsx objects
-  [
-      <h1>Hello world</h1>,
-      <p>Hello</p>,
-      <h2>plz subscribe my channel</h2>,
-      <p>This is array</p>
-  ]
+  // [
+  //     <h1>Hello world</h1>,
+  //     <p>Hello</p>,
+  //     <h2>plz subscribe my channel</h2>,
+  //     <p>This is array</p>
+  // ]
+
+  // React.Fragment 
+  //  <React.Fragment>
+  //     <h1>Hello world</h1>
+  //     <p>Hello</p>
+  //     <h2>plz subscribe my channel</h2>
+  // </React.Fragment>
+  // We can also do this for react.fragment
+  <>
+      <h1>Hello world</h1>
+      <p>Hello</p>
+      <h2>plz subscribe my channel</h2>
+  </>
 ,
   document.getElementById('root')
 );
---
+++ #9 React challenge 01/challengeone/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <>
+    <h1>My Favourite Netflix Serries</h1>
+     <p>Here are my five favorite netflix serries</p>
+     <ol>
+      <li>Lock & Key</li>
+      <li>The Last Airbender</li>
+      <li>Stranger Things</li>
+      <li>Crash Landing on you</li>
+      <li>Vinchenzo</li>
+     </ol>
+  </>,
+  document.getElementById('root')
+)
