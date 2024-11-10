e6bccc9ad4c0407bd26edf2a4083a1a2e5b1f796
ISRA ILYAS <israqureshi138@gmail.com>
2024-11-11 00:32:17 +0500

    challenge two

---
+++ React JS Challenge 02/challengetwo/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mName = "ISRA ILYAS";
+const date = new Date();
+console.log(date)
+
+ReactDOM.render(
+  <>
+    <h1>My name is {mName}</h1>
+    <p>Current date is {date.toDateString()}</p>
+    <p>{`Current time is ${date.getHours()} ${date.getMinutes()} ${date.getSeconds()}`}</p>
+  </>,
+  document.getElementById('root')
+)
+
