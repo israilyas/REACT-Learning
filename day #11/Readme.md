174b474795752f589881c0e177048ad587cae73c
ISRA ILYAS <israqureshi138@gmail.com>
2024-11-11 20:30:59 +0500

    added css in react

--- day #11/Readme.md
+++ day #11/Readme.md
@@ -1,3 +1,11 @@
-## JSX Attributes in ReactJS 
+## 13 JSX Attributes in ReactJS 
 
 - HTML Attribute Vs JSX Attribute
+
+## 14 CSS Styling In React
+
+- import css into index file `import './index.css'; `
+- In react use className instead of class for jsx styling purpose.
+- class keyword is reserved in react.
+
+## 15 Add google fonts in React
--- day #11/day11/src/index.js
+++ day #11/day11/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const mName = "ISRA";
-const image = "https://picsum.photos/200/300";
+const image1 = "https://picsum.photos/200/300";
+const image2 = "https://picsum.photos/250/300";
+const image3 = "https://picsum.photos/300/300";
 const link_youtube = "https://www.youtube.com/watch?v=OloAP8p1k1w&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=14&t=11s"
 
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">My name is {mName}</h1>
-    <img src={image} alt='random images'/>
-    <a href={link_youtube} target='_'>Thapa Technical</a>
+    <h1 className='heading'>My name is {mName}</h1>
+      <div className='imgDiv'>
+      <img src={image1} alt='random images' />
+      <img src={image2} alt='random images' />
+      <a href={link_youtube} target='_'>
+        <img src={image3} alt='random images' />
+  </a >
+      </div>
+      
+<a href={link_youtube} target='_'>Thapa Technical</a>
   </>,
   document.getElementById('root')
 )
