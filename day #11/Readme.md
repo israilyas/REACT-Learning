cc6a2e64eea50465b8720f3259629f653eed79ec
ISRA ILYAS <israqureshi138@gmail.com>
2024-11-11 23:19:16 +0500

    inline css

--- day #11/Readme.md
+++ day #11/Readme.md
@@ -8,4 +8,8 @@
 - In react use className instead of class for jsx styling purpose.
 - class keyword is reserved in react.
 
-## 15 Add google fonts in React
+## 15 Add google fonts in React:
+- add fonts link in index.html
+- use font family in css
+
+## 16 Internal CSS and Inline CSS
--- day #11/day11/src/index.js
+++ day #11/day11/src/index.js
@@ -8,9 +8,22 @@ const image2 = "https://picsum.photos/250/300";
 const image3 = "https://picsum.photos/300/300";
 const link_youtube = "https://www.youtube.com/watch?v=OloAP8p1k1w&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=14&t=11s"
 
+// CSS
+const heading = {
+  color: '#fa9191',
+  textAlign: 'center',
+  textTransform: 'capitalize',
+  fontWeight: 'bold',
+  fontFamily: "Josefin Sans",
+  marginTop:'20px',
+  textShadow: '0px 2px 4px #e71c96',
+  fontFamily: '"Josefin", Sans sans-serif'
+}
+
 ReactDOM.render(
   <>
-    <h1 className='heading'>My name is {mName}</h1>
+  {/* Inline CSS */}
+    <h1 style={heading}>My name is {mName}</h1>
       <div className='imgDiv'>
       <img src={image1} alt='random images' />
       <img src={image2} alt='random images' />
