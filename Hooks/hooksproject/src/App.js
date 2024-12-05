41aec3ca0d15e17c3134f8b7ba468b4432572d6a
ISRA ILYAS <israqureshi138@gmail.com>
2024-12-05 16:17:15 +0500

    added hooks

---
+++ Hooks/hooksproject/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import UseState from './component/Hooks/useState';
+
+const App = () => {
+  return <UseState />
+}
+
+export default App
+
---
+++ Hooks/hooksproject/src/component/Hooks/useState.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./style.css"
+
+const UseState = () => {
+    return (
+        <>
+            <div className='container'>
+                <p>10</p>
+                <div>
+                    <button>INC</button>
+                    <button>DeC</button>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default UseState
